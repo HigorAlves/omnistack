21189e3e27ce25a47872ee01ddc3348e8c0172fe
Higor Alves <higorhaalves@gmail.com>
2023-03-01 00:33:08 -0300

    chore: initial basic config

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-};
+}
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
+
+module.exports = withNextra(nextConfig)
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
