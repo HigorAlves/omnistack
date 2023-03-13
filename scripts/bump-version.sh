e89496f9d0a69bf3ac9cc9341b75ac8ee97063ea
Higor Alves <higorhaalves@gmail.com>
2023-03-13 19:26:23 -0300

    chore: scripts to bump version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
-# shellcheck disable=SC2028
-echo "\n\e[34mCLEANING MONOREPO...\e[0m"
+echo "BUMPING PACKAGES VERSIONS"
+# Check if an argument was passed
+if [ -z "$1" ]; then
+    echo "Please provide a version number in the semantic versioning style (e.g. 1.2.3)"
+    exit 1
+fi
 
-yarn version apply
+# Check if the argument follows semantic versioning style
+if [[ ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "The version number should follow the semantic versioning style (e.g. 1.2.3)"
+    exit 1
+fi
 
-# shellcheck disable=SC2028
-echo "\n\e[32mCLEAN & DEPENDENCIES COMPLETE\e[0m"
+# Update the version number in all package.json files
+find . -name "package.json" -type f -exec sed -i '' "s/\"version\": \".*\"/\"version\": \"$1\"/g" {} +
+
+echo "All package.json files have been updated to version $1"
