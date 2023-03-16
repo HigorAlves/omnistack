832f8b30b88e1ef1a62d33583cde36672bd5d351
Higor Alves <higorhaalves@gmail.com>
2023-03-16 12:26:33 -0300

    fix(omni): lint command updated

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,6 @@ set -e
 echo $"\e[0;34m PRETTYING STAGED FILES...\e[0m"
 
 prettier --write "**/*.{ts,tsx,js,jsx,md,json,yml}"
-eslint --fix --cache --quiet --ext ts,tsx,js,jsx,json,yml "$@"
+eslint --fix --cache --quiet --ext ts,tsx,js,jsx,json,yml --format html --output-file ./reports/eslint.html
 
 echo $"\e[32m FILES ARE NOW PRETTY AGAIN\e[0m"
