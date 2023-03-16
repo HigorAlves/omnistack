cc38d133505bb832bf63d501d5d8adc0aa51a778
Higor Alves <higorhaalves@gmail.com>
2023-03-16 11:32:13 -0300

    chore(omni): fixing the packages version

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@ set -e
 SOURCE_BRANCH=dev
 VERSION_TYPE=$1
 
-echo "preparing release branch..."
+echo "PREPARING RELEASE BRANCH"
 
 # Check if an argument was passed
 if [ -z "$VERSION_TYPE" ]; then
@@ -13,25 +13,29 @@ if [ -z "$VERSION_TYPE" ]; then
 fi
 
 if [[ "$VERSION_TYPE" =~ \b(major|minor|patch)\b ]]; then
-    echo "$VERSION_TYPE is a valid version type"
-    exit 1
+    echo "$VERSION_TYPE IS A VALID VERSION TYPE"
 else
     echo "$VERSION_TYPE is not a valid version type"
+    exit 1
 fi
 
-echo "checking out latest $SOURCE_BRANCH"
+echo "AUDITING YOUR CODE"
+yarn build
+yarn lint:ci
+yarn audit
+
+echo "CHECKING OUT LATEST $SOURCE_BRANCH"
 git checkout $SOURCE_BRANCH
 git pull --rebase
-yarn release:prepare -- --release-as $VERSION_TYPE
+yarn release:prepare -- --release-as "$VERSION_TYPE"
 
 # Read the version number from the package.json file in the current directory
+# shellcheck disable=SC2002
 VERSION_LABEL=v$(cat package.json | grep "\"version\":" | awk -F'"' '{print $4}')
 BRANCH=release/$VERSION_LABEL
 
-echo "creating new release branch $BRANCH from $SOURCE_BRANCH"
-git checkout -b $BRANCH
+echo "CREATING NEW RELEASE BRANCH $BRANCH FROM $SOURCE_BRANCH"
+git checkout -b "$BRANCH"
+git push --set-upstream origin "$BRANCH"
 
-# Set upstream branch so that future push's can occur with `git push`
-git push --set-upstream origin $BRANCH
-
-echo "release prepared!"
+echo "RELEASE PREPARED!"
